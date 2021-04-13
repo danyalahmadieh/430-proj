9d66842411aaa0b2849fe702ebfb2df780aae4da
danyalahmadieh <82466941+danyalahmadieh@users.noreply.github.com>
2021-04-13 22:33:58 +0300

    Add files via upload

--- proj_name/patients/forms.py
+++ proj_name/patients/forms.py
@@ -5,21 +5,22 @@ from django.db import transaction
 from Proj.models import Patients, User
 
 class PatientSignUpForm(UserCreationForm):
-    p_name = forms.CharField()
-    p_email = forms.EmailField()
-    p_dob = forms.DateTimeField()
-    p_phone = forms.CharField()
+    p_name = forms.CharField(label= "Name:")
+    p_email = forms.EmailField(label= "Email:")
+    p_dob = forms.DateTimeField(label= "Date of Birth (YYYY-MM-DD):")
+    p_phone = forms.CharField(label= "Phone #: ")
 
     class Meta(UserCreationForm.Meta):
         model = User
     @transaction.atomic
     def save(self):
         user = super().save(commit=False)
-        user.is_patient = True
+        user.is_patients = True
         user.save()
         patient = Patients.objects.create(user=user)
-        patient.p_email.add(*self.cleaned_data.get('p_email'))
-        patient.p_name.add(*self.cleaned_data.get('p_name'))
-        patient.p_dob.add(*self.cleaned_data.get('p_dob'))
-        patient.p_phone.add(*self.cleaned_data.get('p_phone'))
+        patient.p_email = self.cleaned_data.get('p_email')
+        patient.p_name = self.cleaned_data.get('p_name')
+        patient.p_dob = self.cleaned_data.get('p_dob')
+        patient.p_phone = self.cleaned_data.get('p_phone')
         return user
+          
--- proj_name/patients/views.py
+++ proj_name/patients/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import CreateView
 from django.http import HttpResponse
 from Proj.models import User
@@ -13,7 +13,7 @@ class SignUpView(CreateView):
     def get_context_data(self, **kwargs):
         kwargs['user_type'] = 'Patients'
         return super().get_context_data(**kwargs)
-
     def form_valid(self, form):
         user = form.save()
         login(self.request, user)
+        return redirect('index')
