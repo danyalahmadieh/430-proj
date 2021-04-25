6a004db0b41656d3e4c29e53d76ecbad8cf77ae6
danyalahmadieh <82466941+danyalahmadieh@users.noreply.github.com>
2021-04-25 19:04:13 +0300

    Add files via upload

--- Proj/admin.py
+++ Proj/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
 from django.contrib.auth.admin import UserAdmin
-from .models import User,Patients,Doctors
-
+from .models import User,Patients,Doctors,Meeting
 admin.site.register(Patients)
 admin.site.register(Doctors)
+admin.site.register(Meeting)
 admin.site.register(User,UserAdmin)
